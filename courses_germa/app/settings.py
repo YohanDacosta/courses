419605579bf36b3430fbb62c6868ef0ef29604b6
Yohan910 <yohan.dacosta910@gmail.com>
2025-01-28 21:18:01 +0100

    Configuration DB MySQL

--- courses_germa/app/settings.py
+++ courses_germa/app/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": "courses",
+        "USER": "root",
+        "PASSWORD": "admin",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
